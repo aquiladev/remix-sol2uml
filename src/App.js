c398b40cb0be0c2c906d6b5a90564cd62279fa51
Sergii Bomko <aquila.ua@gmail.com>
2021-01-12 13:59:19 +0200

    update UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { createIframeClient } from '@remixproject/plugin';
 import parser from '@solidity-parser/parser';
 import { convertUmlClassesToSvg } from 'sol2uml';
 import { convertNodeToUmlClass } from 'sol2uml/lib/parser';
-import { useState } from 'react';
 import Viewer from 'react-viewer';
 
 import './App.css';
@@ -11,8 +10,9 @@ import './App.css';
 function App() {
   console.debug('Plugin: remix-sol2uml: starting');
 
-  const [client, setClient] = React.useState(null)
-  const [svg, setSvg] = useState();
+  const [client, setClient] = React.useState(null);
+  const [isClientLoaded, setIsClientLoaded] = React.useState(false);
+  const [svg, setSvg] = useState(undefined);
   
   useEffect(() => {
     async function init() {
@@ -20,6 +20,7 @@ function App() {
         setClient(createIframeClient());
       } else {
         await client.onload();
+        setIsClientLoaded(true);
         console.debug('Plugin: remix-sol2uml: remix client loaded');
 
         client.on('solidity', 'compilationFinished', async (target, source) => {
@@ -27,7 +28,7 @@ function App() {
 
           const ast = parser.parse(source.sources[target].content);
           console.debug('Plugin: remix-sol2uml: AST', ast);
-          
+
           const umlClass = convertNodeToUmlClass(ast, target)
           const solSvg = await convertUmlClassesToSvg(umlClass);
           setSvg(solSvg.substr(solSvg.indexOf('<svg ')));
@@ -40,17 +41,32 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Viewer
-          visible={true}
-          rotatable={false}
-          loop={false}
-          noClose={true}
-          noFooter={true}
-          showTotal={false}
-          changeable={false}
-          zoomSpeed={0.2}
-          minScale={1}
-          images={[{src: `data:image/svg+xml;base64,${btoa(svg)}`}]} />
+        {
+          client ? 
+            <>
+              { isLoaded ?
+                <>
+                  {
+                    svg ?
+                    <Viewer
+                      visible={true}
+                      rotatable={false}
+                      loop={false}
+                      noClose={true}
+                      noFooter={true}
+                      showTotal={false}
+                      changeable={false}
+                      zoomSpeed={0.2}
+                      minScale={1}
+                      images={[{src: `data:image/svg+xml;base64,${btoa(svg)}`}]} /> :
+                    <div>Compile smart contract and come back!</div>
+                  }
+                </> :
+                <div>Waiting for connection to <a href='https://remix.ethereum.org/' className='App-link'>Remix</a></div>
+              }
+            </> :
+            <div>Loading...</div>
+        }
       </header>
     </div>
   );
